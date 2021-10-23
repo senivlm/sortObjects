80cfa83c516cfbabdb252d8a4fed87c358a56bc8
ak-parser <81529142+ak-parser@users.noreply.github.com>
2021-10-23 16:47:43 +0300

    Complited task

---
+++ Product.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortObject
+{
+    //Summary:
+    //    Contains data about product
+    class Product
+    {
+        private string name;
+        private double price;
+        private double weight;
+        private int expirationDate;
+        private DateTime createDateTime;
+
+        public string Name {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException("The product name cannot be empty");
+                else
+                    name = value;
+            }
+        }
+        public double Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentException("The product price cannot be <= 0");
+                else
+                    price = value;
+            }
+        }
+        public double Weight
+        {
+            get
+            {
+                return weight;
+            }
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentException("The product weight cannot be <= 0");
+                else
+                    weight = value;
+            }
+        }
+
+        public int ExpirationDate
+        {
+            get
+            {
+                return expirationDate;
+            }
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentException("Invalid expiration date");
+                else
+                    expirationDate = value;
+            }
+        }
+
+        public DateTime CreateDateTime
+        {
+            get
+            {
+                return createDateTime;
+            }
+            set
+            {
+                if (value == null)
+                    throw new ArgumentNullException("Create date time cannot be null");
+                else
+                    createDateTime = value;
+            }
+        }
+
+        //Exceptions:
+        //    ArgumentException
+        //    ArgumentNullException
+        public Product(string name, double price, double weight, int expirationDate, DateTime dateTime)
+        {
+            Name = name;
+            Price = price;
+            Weight = weight;
+            ExpirationDate = expirationDate;
+            CreateDateTime = dateTime;
+        }
+
+        //Summary:
+        //    Checks if name both Product are the same
+        public override bool Equals(object obj)
+        {
+            if (obj.GetType() == typeof(Product))
+            {
+                Product product = (Product)obj;
+                if (product.Name == Name)
+                    return true;
+            }
+
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            string info = $"Name: {Name} Price: {Price} Weight: {Weight} Expiration date: {ExpirationDate} " +
+                $"Create date: {CreateDateTime.Day}.{CreateDateTime.Month}.{CreateDateTime.Year}\n";
+
+            return info;
+        }
+
+        //Exceptions:
+        //    ArgumentException
+        virtual public void ChangePrice(double percent)
+        {
+            if ((percent <= -100) && (int.MaxValue / Price) <= (percent / 100))
+                throw new ArgumentException("Percent is too small or too large");
+            else
+                Price *= 1 + percent / 100;
+        }
+
+        //Summary:
+        //    Initialize object with data fro string
+        //Exceptions:
+        //    ArgumentException
+        virtual public void Parse(string s)
+        {
+            string[] inputData = s.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            if (inputData.Length != 5)
+                throw new ArgumentException("Data in line is incorrect");
+
+            Name = inputData[0];
+            Price = Convert.ToDouble(inputData[1]);
+            Weight = Convert.ToDouble(inputData[2]);
+            ExpirationDate = Convert.ToInt32(inputData[3]);
+
+            string[] dateList = inputData[4].Split(".");
+            CreateDateTime = new DateTime(Convert.ToInt32(dateList[2]), Convert.ToInt32(dateList[1]), Convert.ToInt32(dateList[0]));
+        }
+    }
+}
---
+++ SortClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortObject
+{
+    static class SortClass
+    {
+        public delegate int Compare(object obj1, object obg2);
+
+        //Summary:
+        //    Sorts objects array with bubble sort method
+        public static void Sort(object[] obj, Compare compare)
+        {
+            if (obj == null)
+                throw new ArgumentNullException("Object array cannot be null");
+
+            for (int i = 0; i < obj.Length; i++)
+                for (int j = 0; j < obj.Length - i - 1; j++)
+                    if (compare(obj[j], obj[j + 1]) > 0)
+                    {
+                        object temp = obj[j];
+                        obj[j] = obj[j + 1];
+                        obj[j + 1] = temp;
+                    }
+        }
+    }
+}
